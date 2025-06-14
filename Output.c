2a2ab5df41655390ca28b82e2916b828b7489184
JhiteshGuptha <jhiteshguptha.1785@gmail.com>
2025-06-14 15:17:28 +0530

    Added Output File for Reference

---
+++ Output.c
@@ -0,0 +1,24 @@
+/*
+*Learning how to display output in C
+*printf is the built in function used to display output
+*/
+
+#include<stdio.h>
+int main() {
+
+    // Standard Output
+
+    printf("Welcome to Code.io");
+    printf("1234");
+
+    // Escape Sequence with Printf
+
+    printf("\nWe are learning about Output\n");
+    printf("\t Hope you are having Fun!\n");
+
+    // Multiple Outputs 
+
+    printf("We learned Standard output\n");
+    printf("Output with escape sequence"); 
+    return 0;
+}
